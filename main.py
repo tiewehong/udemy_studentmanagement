b59a122dcd4fe7ee1b79dc19af46def4b40f2177
tiewehong <37630980+tiewehong@users.noreply.github.com>
2024-08-11 16:42:10 +1000

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QTableWidget
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -18,11 +19,28 @@ class MainWindow(QMainWindow):
         about_action =  QAction("About", self)
         help_menu_item.addAction(about_action)
 
-        table = QTableWidget()
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+
 
 
 app = QApplication(sys.argv)
 management_app = MainWindow()
 management_app.show()
+management_app.load_data()
 sys.exit(app.exec())
 
